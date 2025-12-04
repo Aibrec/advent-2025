2967612b8ed0bcff56c201d0c64a45f553c9602e
James <jamesmcneice@gmail.com>
2025-12-03 22:52:34 -0700

    Set version, also faster

--- Day 4b/main.py
+++ Day 4b/main.py
@@ -1,11 +1,11 @@
 import time
 
 def get_adjacent(coords):
-    adjacent = []
+    adjacent = set()
     for y in (-1,0,1):
         for x in (-1,0,1):
             if x != 0 or y != 0:
-                adjacent.append((coords[0]+y, coords[1]+x))
+                adjacent.add((coords[0]+y, coords[1]+x))
     return adjacent
 
 def get(grid, coord):
@@ -14,53 +14,45 @@ def get(grid, coord):
             return grid[coord[0]][coord[1]]
     return None
 
-def remove_paper(grid, coords_to_consider):
-    papers_removed = set()
-    coords_to_consider_next_time = set()
-    for coord in coords_to_consider:
-        adjacent_coords = get_adjacent(coord)
-        adjacent_values = list([get(grid, adj_coord) for adj_coord in adjacent_coords])
-        if adjacent_values.count('@') < 4:
-            grid[coord[0]][coord[1]] = '.'
-            papers_removed.add(coord)
-            for adj_coord in adjacent_coords:
-                if get(grid, adj_coord) == '@': # Should use the values we already got somehow
-                    coords_to_consider_next_time.add(adj_coord)
+def remove_paper(papers, papers_to_consider):
+    papers_to_consider_next_time = set()
+    for coord in papers_to_consider:
+        adj_coords = get_adjacent(coord)
+        adj_papers = adj_coords.intersection(papers)
+        if len(adj_papers) < 4:
+            papers.remove(coord)
+            papers_to_consider_next_time.update(adj_papers)
 
-    coords_to_consider_next_time -= papers_removed
-    return papers_removed, coords_to_consider_next_time
-
-def find_papers(grid):
-    papers = set()
-    for y, row in enumerate(grid):
-        for x, cell in enumerate(row):
-            if cell == '@':
-                papers.add((y,x))
-    return papers
+    papers_to_consider_next_time = papers_to_consider_next_time.intersection(papers)
+    return papers, papers_to_consider_next_time
 
 def main():
     file_path = 'input.txt'
 
     start_time = time.perf_counter()
+    papers = set()
     with open(file_path, 'r') as file:
-        grid = []
+        y = 0
         for line in file:
             line = line.strip()
-            grid.append(list([c for c in line]))
+            for x, char in enumerate(line):
+                if char == '@':
+                    papers.add((y,x))
+            y += 1
 
-    accessible_paper = 0
-    coords_to_consider = find_papers(grid)
+    starting_papers = len(papers)
+    coords_to_consider = papers.copy()
     while True:
-        papers_removed, coords_to_consider = remove_paper(grid, coords_to_consider)
-        if papers_removed:
-            accessible_paper += len(papers_removed)
-        else:
+        papers, coords_to_consider = remove_paper(papers, coords_to_consider)
+        if not coords_to_consider:
             break
 
     end_time = time.perf_counter()
-    print(f"accessible_paper is {accessible_paper}")
+    print(f"accessible_paper is {starting_papers - len(papers)}")
     time_in_microseconds = (end_time - start_time) * 1000000
     print(f"took {time_in_microseconds:.2f}μs")
-
+    # brute version took something like 3000000
+    # faster version took 59925.70μs
+    # set version took 35959.80μs
 main()
 
